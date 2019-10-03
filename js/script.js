37583c00d3fb040a31fe70d61f6484834bd4712b
Firefox3000 <Firefox3000@users.noreply.github.com>
2019-10-03 09:57:42 +0200

    more SVG

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function toggleNav() {
     document.querySelector('.burgerMenu').classList.toggle('menuActive');
+    document.querySelector('nav').classList.toggle('menuActive');
 }
 
 document.querySelector('.burgerMenu').addEventListener('click', toggleNav)
