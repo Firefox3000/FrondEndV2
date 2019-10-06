f9fe472342da5ef9701baf2122af784d9610461c
Firefox3000 <Firefox3000@users.noreply.github.com>
2019-10-06 22:41:14 +0200

    meer front page
    List items

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+function setup() {
+    for (let i = 0; i < document.querySelectorAll('.favIcon').length; i++) {
+        document.querySelectorAll('.favIcon')[i].addEventListener('click', favorite);
+    }
+}
+
+setup()
+
 function toggleNav() {
     document.querySelector('.burgerMenu').classList.toggle('menuActive');
     document.querySelector('nav').classList.toggle('menuActive');
 }
 
 document.querySelector('.burgerMenu').addEventListener('click', toggleNav)
+
+
+function favorite(e) {
+    e.currentTarget.classList.toggle('favIconActive');
+}
