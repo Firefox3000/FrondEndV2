0e0acb23e265b0611b39a45c06bdc19189d9fdc2
Sam de Man <47493725+Firefox3000@users.noreply.github.com>
2019-10-07 19:14:32 +0200

    yoink js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const weatherApiKey = '&units=metric&APPID=d2eb5b400421ec9aceb37cab13852517';
+const weatherApiUrl = 'api.openweathermap.org/data/2.5/weather?';
+
+const hikeApiUrl = 'https://www.hikingproject.com/data/';
+const hikeApiKey = '&key200612053-f2907f0c887fb6db20bc6ef07aacbced';
+
+var weatherData;
+var hikeData;
+
+var lon;
+var lat;
+
+
 function setup() {
     for (let i = 0; i < document.querySelectorAll('.favIcon').length; i++) {
         document.querySelectorAll('.favIcon')[i].addEventListener('click', favorite);
@@ -17,3 +30,66 @@ document.querySelector('.burgerMenu').addEventListener('click', toggleNav)
 function favorite(e) {
     e.currentTarget.classList.toggle('favIconActive');
 }
+
+
+async function getApi() {
+    console.log(document.querySelector('.zoekLocatie').value);
+    let api1 = weatherApiUrl + document.querySelector('.zoekLocatie').value + weatherApiKey;
+    console.log(api1);
+    
+    weatherData = await fetch(api1);
+    weatherData = await weatherData.json();
+    console.log(weatherData);
+
+    lat = weatherData.coord.lat;
+    lon = weatherData.coord.lon;
+
+    console.log ('lat= ' + lat + ' lon= '+ lon);
+
+    let api2 = hikeApiUrl + 'get-trails?lat=' + lat + '&lon=' + lon + hikeApiKey;
+    console.log(api2);
+
+    hikeData = await fetch(api2);
+    hikeData = await hikeData.json();
+    console.log(weatherData);
+
+    // get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200612053-f2907f0c887fb6db20bc6ef07aacbced
+};
+
+console.log('active')
+
+/*
+async function getHikeApi() {
+    console.log(document.querySelector('.locatieZoek').value);
+
+
+
+    // const responce = await fetch();
+
+
+/*
+
+        let bestaat = false;
+        let jsonUrl2 = jsonUrl + guess.charAt(0).toLowerCase() + '.json';
+        const responce = await fetch(jsonUrl2);
+        let data = await responce.json();
+    
+        for (i = 0; i < data.length; i++) {
+            if (data[i].toLowerCase() == guess.toLowerCase()) {
+                bestaat = true;
+                console.log('dit woord bestaat');
+            }
+        }
+        return bestaat;
+        */
+// }
+
+document.querySelector('.submitZoeken').addEventListener('click', function(event) {
+    event.preventDefault();
+});
+
+document.querySelector('.submitZoeken').addEventListener('click', getApi);
+
+
+
+
